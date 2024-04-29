30ee611c2a9415a3ccfbfc92e9f6609be0717e40
yudafatoni <fatoniyuda02@gmail.com>
2024-04-29 09:23:20 +0700

    frontend navbar, home, about

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,9 +5,98 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Classic Barber</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montaga&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Montaga&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+        </style>
 </head>
 <body>
-    <h1 class="fw-bold bg-primary ">Yuda</h1>
+    <header>
+        <nav>
+            <nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+                <div class="container">
+                    <a class="navbar-brand" href="#">
+                        <img src="assets/logoBarber.svg" alt="" width="94" height="83">
+                      </a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">About</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Kursus</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Produk</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Galeri</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Artikel</a>
+                      </li>
+                      
+                    </ul>
+                    <form class="d-flex ">
+                        <div class="icon mx-5">
+                            <img src="assets/iconYoutube.png" alt="iconyoutube"  width="24" height="25">
+                            <img src="assets/iconInstagram.png" alt="iconinstagram" width="24" height="25">
+                            <img src="assets/iconTiktok.png" alt="icontiktok" width="24" height="25">
+                        </div>
+                      <button class="btn-book mx-2" type="submit">Book Now</button>
+                      <button class="btn-login" type="submit">Login</button>
+                    </form>
+                  </div>
+                </div>
+              </nav>
+        </nav>
+    </header>
+
+    <main>
+        <section id="home">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <h1>Sharp Cuts, Sharp Looks: Unleash Your Style</h1>
+                        <h3>At Classic Barber’s</h3>
+                            <div class="link d-flex ">
+                                 <button class="btn-home" type="submit">Look More</button>
+                                <div class="location">
+                                 <a href="/" style="color: white">Our Location</a>
+                                </div>
+                            </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <img src="assets/imghome.png" alt="icontiktok" width="513" height="486">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="about">
+            <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <img src="assets/logoBarber.svg" alt="" width="644" height="574">
+                </div>
+                <div class="col-lg-6">
+                    <p><span>Lorem ipsum</span> dolor sit amet consectetur adipisicing elit. Optio laboriosam velit dolorem a alias! Provident explicabo quaerat incidunt ipsa voluptatibus, laborum tempora temporibus accusantium harum. Asperiores quas iure id ea!</p>
+                    <div class="icon-about">
+                        <img src="assets/iconYoutube.png" alt="iconyoutube"  width="24" height="25">
+                        <img src="assets/iconInstagram.png" alt="iconinstagram" width="24" height="25">
+                        <img src="assets/iconTiktok.png" alt="icontiktok" width="24" height="25">
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+    </main>
 
 
 

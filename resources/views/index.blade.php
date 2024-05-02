4ab7d487510280a3c7412ea25348036ddaa5faaa
yudafatoni <fatoniyuda02@gmail.com>
2024-05-02 17:07:08 +0700

    landing page lanjut

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Classic Barber</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="shortcut icon" href="/assets/logobarber.svg">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montaga&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/style.css">
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montaga&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
-
-        </style>
+    </style>
 </head>
+
 <body>
     <header>
         <nav>
@@ -21,86 +24,225 @@
                 <div class="container">
                     <a class="navbar-brand" href="#">
                         <img src="assets/logoBarber.svg" alt="" width="94" height="83">
-                      </a>
-                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                  </button>
-                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                      <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">About</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Kursus</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Produk</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Galeri</a>
-                      </li>
-                      <li class="nav-item">
-                        <a class="nav-link" href="#">Artikel</a>
-                      </li>
-                      
-                    </ul>
-                    <form class="d-flex ">
-                        <div class="icon mx-5">
-                            <img src="assets/iconYoutube.png" alt="iconyoutube"  width="24" height="25">
-                            <img src="assets/iconInstagram.png" alt="iconinstagram" width="24" height="25">
-                            <img src="assets/iconTiktok.png" alt="icontiktok" width="24" height="25">
-                        </div>
-                      <button class="btn-book mx-2" type="submit">Book Now</button>
-                      <button class="btn-login" type="submit">Login</button>
-                    </form>
-                  </div>
+                    </a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">HOME</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">SERVICES</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">PRODUCT</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    INFORMATION
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">COURSE</a></li>
+                                    <li><a class="dropdown-item" href="#">FRANCHISE</a></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">OUR GALLERY</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">CONTACT US</a>
+                            </li>
+
+                        </ul>
+                        <form class="d-flex mt-5">
+                            <button class="btn-login" type="submit">BOOK NOW</button>
+                        </form>
+                    </div>
                 </div>
-              </nav>
+            </nav>
         </nav>
     </header>
 
     <main>
-        <section id="home">
-            <div class="container">
+        <section id="home ">
+            <div class="container min-vh-100">
                 <div class="row">
-                    <div class="col-lg-7">
-                        <h1>Sharp Cuts, Sharp Looks: Unleash Your Style</h1>
-                        <h3>At Classic Barber’s</h3>
-                            <div class="link d-flex ">
-                                 <button class="btn-home" type="submit">Look More</button>
-                                <div class="location">
-                                 <a href="/" style="color: white">Our Location</a>
-                                </div>
-                            </div>
-                    </div>
                     <div class="col-lg-5">
-                        <img src="assets/imghome.png" alt="icontiktok" width="513" height="486">
+                        <img src="assets/logoBarber.png" alt="logo barber" width="535" height="406">
+                    </div>
+                    <div class="col-lg-7">
+                        <h1>Tampil lebih Fresh Dengan harga terjangkau</h1>
+                        <h3>Ingat cukur, ingat Classic Barber’s</h3>
+                        <div class="link d-flex ">
+                            <button class="btn-login" type="submit">BOOK NOW</button>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </section>
-        <section id="about">
-            <div class="container">
-            <div class="row">
-                <div class="col-lg-6">
-                    <img src="assets/logoBarber.svg" alt="" width="644" height="574">
-                </div>
-                <div class="col-lg-6">
-                    <p><span>Lorem ipsum</span> dolor sit amet consectetur adipisicing elit. Optio laboriosam velit dolorem a alias! Provident explicabo quaerat incidunt ipsa voluptatibus, laborum tempora temporibus accusantium harum. Asperiores quas iure id ea!</p>
-                    <div class="icon-about">
-                        <img src="assets/iconYoutube.png" alt="iconyoutube"  width="24" height="25">
-                        <img src="assets/iconInstagram.png" alt="iconinstagram" width="24" height="25">
-                        <img src="assets/iconTiktok.png" alt="icontiktok" width="24" height="25">
+
+        <section id="services">
+            <div class="imgservices">
+                <img src="assets/bgServices.png" alt="imgServices">
+            </div>
+            <div class="container-fluid services">
+                <h2 class="title-services">Our Services</h2>
+                <p class="text-services">Kami menyediakan beberapa servis, coba pilih sesuai kebutuhan</p>
+                <div class="img-service container">
+                    <div class="row">
+                        <div class="services col-lg-4">
+                            <img src="assets/haircut.png" alt="haircut" width="204" height="206">
+                            <h3>Haircut</h3>
+                        </div>
+                        <div class="services col-lg-4">
+                            <img src="assets/perm.png" alt="perm" width="204" height="206">
+                            <h3>Perm</h3>
+                        </div>
+                        <div class="services col-lg-4">
+                            <img src="assets/smoothing.png" alt="smoothing" width="204" height="206">
+                            <h3>Smoothing</h3>
+                        </div>
+                        <div class="services col-lg-4">
+                            <img src="assets/haircut.png" alt="haircut" width="204" height="206">
+                            <h3>Coloring</h3>
+                        </div>
+                        <div class="services col-lg-4">
+                            <img src="assets/perm.png" alt="perm" width="204" height="206">
+                            <h3>HairMask</h3>
+                        </div>
+                        <div class="services col-lg-4">
+                            <img src="assets/smoothing.png" alt="smoothing" width="204" height="206">
+                            <h3>Home Service</h3>
+                        </div>
+
                     </div>
                 </div>
             </div>
-        </div>
+            </div>
         </section>
+
+        <section id="hairstyle" style="min-height: 90vh">
+            <div class="title">
+                <p class="hairstyle">Our Hair Style</p>
+            </div>
+            <div class="container-fluid">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-2">
+                        <div class="card">
+                            <img src="/assets/gambar1.png" alt="">
+                            <h3 class="text-center">Comma Hair</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card">
+                            <img src="/assets/gambar1.png" alt="">
+                            <h3 class="text-center">Comma Hair</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card">
+                            <img src="/assets/gambar1.png" alt="">
+                            <h3 class="text-center">Comma Hair</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card">
+                            <img src="/assets/gambar1.png" alt="">
+                            <h3 class="text-center">Comma Hair</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card">
+                            <img src="/assets/gambar1.png" alt="">
+                            <h3 class="text-center">Comma Hair</h3>
+                        </div>
+                    </div>
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-2">
+                        <div class="card">
+                            <img src="/assets/gambar1.png" alt="">
+                            <h3 class="text-center">Comma Hair</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card">
+                            <img src="/assets/gambar1.png" alt="">
+                            <h3 class="text-center">Comma Hair</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card">
+                            <img src="/assets/gambar1.png" alt="">
+                            <h3 class="text-center">Comma Hair</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card">
+                            <img src="/assets/gambar1.png" alt="">
+                            <h3 class="text-center">Comma Hair</h3>
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="card">
+                            <img src="/assets/gambar1.png" alt="">
+                            <h3 class="text-center">Comma Hair</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="location" style="min-height: 100vh">
+            <div class="title">
+                <p>Our Locations</p>
+            </div>
+
+            </div>
+        </section>
+
+        <section id="course" style="min-height: 100vh">
+            <div class="title">
+                <p>Classic Course</p>
+            </div>
+
+            </div>
+        </section>
+        <section id="hairartist" style="min-height: 100vh">
+            <div class="title">
+                <p>Hair Artist</p>
+            </div>
+
+            </div>
+        </section>
+        <section id="product" style="min-height: 100vh">
+            <div class="title">
+                <p>Product</p>
+            </div>
+
+            </div>
+        </section>
+        <footer>
+            <div class="footer container-fluid">
+                <h1 class="text-center">footer</h1>
+
+            </div>
+        </footer>
+
+
     </main>
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>

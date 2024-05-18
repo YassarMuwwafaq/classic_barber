3c27121d9684d141d07c0497323f8f6e4a6594b7
Yassar Muwwafaq Malyan Shabira <yassaricha@gmail.com>
2024-05-18 13:56:40 +0700

    update booking page

--- resources/views/bookingBarber.blade.php
+++ resources/views/bookingBarber.blade.php
@@ -5,11 +5,14 @@
     <div class="section">
         <h4 class="text-center mb-4 text-white">Silahkan pilih cabang</h4>
         <!-- Memindahkan tulisan di atas card dan menengahkannya -->
-        <div class="cabang">
+        <div class="row">
             @foreach ($barbers as $barber)
-                <!-- card -->
-                <div class="card" style="width: 18rem;">
-                    <img src="{{ asset('storage/' . $barber->foto) }}" alt="{{ $barber->nama }}">
+            <!-- card -->
+            <div class="col-md-3 mb-4">
+                <div class="card h-100">
+                    <div class="card-img-top-wrapper">
+                        <img src="{{ asset('storage/'.$barber->foto) }}" class="card-img-top img-fluid" alt="{{ $barber->nama }}">
+                    </div>
                     <div class="card-body d-flex flex-column justify-content-between">
                         <div>
                             <h5>{{ $barber->nama }}</h5>
@@ -18,6 +21,7 @@
                         <a href="{{ route('bookinglayanan', ['barber_id' => $barber->id]) }}" class="btn">Pilih</a>
                     </div>
                 </div>
+            </div>
             @endforeach
         </div>
 

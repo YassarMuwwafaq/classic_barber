6a69a7c5bae83d892ec5f97fe8a80b5673c05f72
yudafatoni <fatoniyuda02@gmail.com>
2024-05-28 20:59:47 +0700

    merge

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('index');
 });
-Route::get('/tes', function () {
-    return view('tesindex');
-});
+
 
 Route::get('/teslogin', function () {
     return view('login');
